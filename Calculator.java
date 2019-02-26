30886ac80771b9c82d82a31f640436bad0d9e6f6
ksrahul99 <47987920+ksrahul99@users.noreply.github.com>
2019-02-25 18:45:53 -0700

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,95 @@
+/**
+ * Name: Rahul Kancharala
+ * Class ID: 431
+ * Assignment 2
+ * Description: This class is given to the students to
+ * 				practice using a version control system. It is a 
+ * 				calculator class which needs to be documented
+ * 				according to the JavaDoc format and indented properly 
+ * 				with structured programming.
+ */
+
+/**
+ * The link to the GitHub site for the following assignment is below
+ * 
+ */
+
+/**
+ * This connects the project and specifies the package.
+ */
+
+package cse360assign2;
+
+/**
+ * Declaring the class Calculator
+ */
+
+public class Calculator {
+
+	/*
+	 * This variable of integer type is used to store the result of 
+	 * all the operations.
+	 */
+	private int total;
+	
+	/*
+	 * This constructor is used to intialize the variable total to 0.
+	 */
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/*
+	 * This is a accessor method.
+	 * @return 0
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/*
+	 * The given method adds the parameter "value" to the 
+	 * integer variable "total"
+	 * @param value
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/*
+	 * The given method subtracts the given parameter 'value'
+	 * from the integer variable "total".
+	 * @param value
+	 */
+	public void subtract (int value) {
+		
+	}
+	
+	/*
+	 * The given method multiplies the variable "total" by
+	 * the given parameter "value".
+	 * @param value
+	 */
+	public void multiply (int value) {
+		
+	}
+	
+	/*
+	 * This method divides the variable total by
+	 * given parameter "value".
+	 * @param value
+	 */
+	public void divide (int value) {
+		
+	}
+	
+	/*
+	 * 
+	 * @return ""
+	 */
+	public String getHistory () {
+		return "";
+	}
+	
+
+}
